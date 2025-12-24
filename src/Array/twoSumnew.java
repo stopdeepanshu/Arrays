2ce1174454001760c54a7a0bae849c644fab7c20
stopdeepanshu <deepanshusinghbanswal20148@gmail.com>
2025-12-25 01:27:32 +0530

    twoSum program

---
+++ src/Array/twoSumnew.java
@@ -0,0 +1,25 @@
+package Array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class twoSumnew {
+	public static void main(String[] args) {
+		int[] arr= {2,4,6,3,5,8};
+		int target=10;
+		
+		Map<Integer, Integer> map=new HashMap<Integer, Integer>();
+		
+		for (int i = 0; i < arr.length; i++) {
+			int required=target-arr[i];
+			
+			if(map.containsKey(required)) {
+				System.out.println(arr[i]+" "+arr[map.get(required)]);
+				
+			}else {
+				map.put(arr[i], i);
+			}
+		}
+		
+	}
+}
